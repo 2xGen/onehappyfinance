0bf78a1874c0354b236be3bca3474d57e0b018fb
Your Name <your.email@example.com>
2025-10-30 15:59:12 +0100

    Update

--- website/middleware.js
+++ website/middleware.js
@@ -51,8 +51,8 @@ export const config = {
     // - _next/static (static files)
     // - _next/image (image optimization files)
     // - favicon.ico (favicon file)
-    // - public files
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    // - public files and sitemaps/robots
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|sitemap.xml.gz|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
 
